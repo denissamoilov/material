eeb9f7fac9f988a70f763788f9e1551c6a28570f
Denis Samoilov <denis@hebsdigital.com>
2019-04-10 17:49:13 +0300

    Added promotions container and withErrorHandler HOC

---
+++ src/containers/ContentManager/Promotions/Promotions.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography';
+
+class Promotions extends Component {
+    render() {
+        return (
+            <Typography variant="h1">Promotions</Typography>
+        )
+    }
+}
+
+export default Promotions;
